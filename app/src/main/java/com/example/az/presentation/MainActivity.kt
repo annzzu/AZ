7eee8e438c90e93cd1b90f13e163c85c949b5539
ana.zurabashvili <az@innotec.ge>
2021-12-24 05:57:39 +0400

    working on bottom app bar

--- app/src/main/java/com/example/az/presentation/MainActivity.kt
+++ app/src/main/java/com/example/az/presentation/MainActivity.kt
@@ -3,45 +3,62 @@ package com.example.az.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log.d
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.example.az.R
 import com.example.az.databinding.ActivityMainBinding
-import com.google.android.material.navigation.NavigationBarView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        initNavBar()
+
+        //        initNavBar()
 
     }
 
-    private fun initNavBar() {
-        NavigationBarView.OnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.item1 -> {
-                    // Respond to navigation item 1 click
-                    d("click", "1")
-                    true
-                }
-                R.id.item2 -> {
-                    d("click", "2")
-                    // Respond to navigation item 2 click
-                    true
-                }
-                R.id.item3 -> {
-                    d("click", "3")
-                    // Respond to navigation item 2 click
-                    true
-                }
-                else -> false
-            }
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        when (item.itemId) {
+//            R.id.navigation_home -> d("click" , "Fav menu item  navigation_home is clicked!")
+//            R.id.navigation_airports -> d(
+//                "click" ,
+//                "Fav menu item  navigation_airports is clicked!"
+//            )
+//        }
+//
+//        return true
+//    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.navigation_home -> toast("navigation_home is clicked!")
+            R.id.navigation_airports -> toast("navigation_airports is clicked!")
         }
+
+        return true
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.bottom_nav_menu , menu)
+        return true
+    }
+
+    private fun toast(test: String) {
+        Toast.makeText(this , test , Toast.LENGTH_SHORT).show()
+    }
 }
