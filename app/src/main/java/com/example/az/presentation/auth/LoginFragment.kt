c76c1ed6b2dadc5834c83a25ff0f20d17b0e4749
ana.zurabashvili <az@innotec.ge>
2021-12-26 05:00:49 +0400

    add home fragment

--- app/src/main/java/com/example/az/presentation/auth/LoginFragment.kt
+++ app/src/main/java/com/example/az/presentation/auth/LoginFragment.kt
@@ -1,5 +1,6 @@
 package com.example.az.presentation.auth
 
+import android.util.Log.d
 import com.example.az.databinding.FragmentLoginBinding
 import com.example.az.extensions.STRINGS
 import com.example.az.presentation.base.BaseFragment
@@ -9,8 +10,11 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate) {
     private lateinit var pagerAdapters: LoginAdapter
+    var number: Int = 0
     override fun init() {
         initTabFragment()
+        number = number + 1
+        d("anuki" , "$number")
     }
 
     private fun initTabFragment() {
@@ -25,5 +29,6 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::i
 
         }
     }
+
 
 }
